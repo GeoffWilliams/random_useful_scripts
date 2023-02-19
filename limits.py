0dea3490116a09bb2a6b546e194e01b41642725d
Geoff Williams <gwilliams@confluent.io>
2023-02-20 09:33:30 +1100

    whoops

--- limits.py
+++ limits.py
@@ -11,35 +11,20 @@ import argparse
 import datetime
 import requests
 import logging
+import pprint
 from string import Template
 
 
 L = logging.getLogger(__name__)
 
-metrics_request_template="""{
-  "aggregations": [
-    {
-      "metric": "io.confluent.kafka.server/$metric_name"
-    }
-  ],
-  "filter": {
-    "field": "resource.kafka.id",
-    "op": "EQ",
-    "value": "$cluster_id"
-  },
-  "granularity": "PT1M",
-  "intervals": [
-    "$interval"
-  ],
-  "limit": 1
-}"""
-
-metrics_endpoint = "https://api.telemetry.confluent.cloud/v2/metrics/cloud/query"
+usage_limits_endpoint = "https://confluent.cloud/api/usage_limits?byok=false"
+env_info_endpoint = "https://confluent.cloud/api/cmk/v2/clusters?environment="
 
 def main():
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('--apikey', dest='apikey', help='Confluent Cloud APIKEY', required=True)
     parser.add_argument('--apisecret', dest='apisecret', help='Confluent Cloud APISECRET', required=True)
+    parser.add_argument('--environment-id', dest='environment_id', help="Environment ID to get limits from, eg env-xg59z", required=True)
     parser.add_argument('--cluster-id', dest='cluster_id', help="Cluster ID to get limits from, eg lkc-w9ykm", required=True)
     parser.add_argument('--verbose', dest='verbose', default=False, action='store_true', help="Print debug info")
 
@@ -48,38 +33,46 @@ def main():
     if args.verbose:
       logging.basicConfig(level=logging.DEBUG)
       L.debug(args)
+
+
+    # 1 - get platform usage limits
+    response = requests.get(usage_limits_endpoint, auth=(args.apikey, args.apisecret))
+    platform_usage_limits = response.json()
+    L.debug("loaded platform limits OK")
+    if args.verbose:
+      pprint.pprint(platform_usage_limits)
+
+    # 2 - get tier info for all clusters hosted in the environment
+    response = requests.get(env_info_endpoint + args.environment_id, auth=(args.apikey, args.apisecret))
+    environment_info = response.json()
+    L.debug("loaded environment info OK")
+    if args.verbose:
+      pprint.pprint(environment_info)
+
+    # 3 - Extract the cluster type - its at data[n][spec][config][kind]
+    cluster_info = [cluster for cluster in environment_info["data"] if cluster["id"] == args.cluster_id]
+    if cluster_info:
+      L.debug(f"found cluster: {cluster_info}")
+    else:
+      raise RuntimeError(f"No such cluster: {args.cluster_id}")
     
+    # 4 - Extract the cluster type - its at data[n][spec][config][kind]
+    cluster_type = cluster_info[0]["spec"]["config"]["kind"]
+    L.debug(f"found cluster type: {cluster_type}")
 
-    # Example of how to discover system-wide available metrics
-    #curl 'https://api.telemetry.confluent.cloud/v2/metrics/cloud/descriptors/metrics?resource_type=kafka' --user "$TOKEN"
+    # 5 - Extract the limits for this type of cluster
+    cluster_usage_limits = platform_usage_limits["usage_limits"]["tier_limits"][cluster_type.upper()]
+    if cluster_usage_limits:
+      L.debug("found cluster usage limits!")
+      if args.verbose:
+        pprint.pprint(cluster_usage_limits)
 
-    # CC UI <--> Metrics API mapping
-    # ==============================
-    #   Ingress = received_bytes
-    #	  Egress = sent_bytes
-    # 	Total client connections = active_connection_count
-    #	  Requests = request_count
-    want_metrics = ["received_bytes"] #, "sent_bytes", "active_connection_count", "request_count"]
+      for limit_name, limit_data in cluster_usage_limits["cluster_limits"].items():
+        limit_unit = limit_data['unit'] if limit_data['unit'] != "UNDEFINED" else ""
+        print(f"{limit_name:<40} {limit_data['value']}{limit_unit}")
 
-    # https://stackoverflow.com/a/28147286/3441106
-    time_now_string = datetime.datetime.now().astimezone().replace(microsecond=0).isoformat()
-    time_one_hour_ago_string = (datetime.datetime.now() - datetime.timedelta(hours = 1)).astimezone().replace(microsecond=0).isoformat()
-    interval = f"{time_one_hour_ago_string}/{time_now_string}"
-    for metric_name in want_metrics:
-      L.debug(f"getting metric: {metric_name}")
-
-      payload = Template(metrics_request_template).substitute({
-        "metric_name": metric_name, 
-        "cluster_id": args.cluster_id,
-        "interval": interval
-      })
-      L.debug(payload)
-
-
-      response = requests.post(metrics_endpoint, data=payload, auth=(args.apikey, args.apisecret), headers={'Content-type': 'application/json'})
-      L.debug(response)
-      L.debug(response.json())
-
+    else:
+      raise RuntimeError("No cluster limits available for cluster type: {cluster_type}")
 
 if __name__ == "__main__":
     main()
